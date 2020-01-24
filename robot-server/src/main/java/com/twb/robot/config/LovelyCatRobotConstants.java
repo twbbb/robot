fadec36e3fd2c0776e4955722571b96d87f3ee5e
Administrator <Administrator@windows10.microdone.cn>
2020-01-24 22:36:14 +0800

    提交

---
+++ robot-server/src/main/java/com/twb/robot/config/LovelyCatRobotConstants.java
@@ -0,0 +1,26 @@
+package com.twb.robot.config;
+
+public class LovelyCatRobotConstants {
+
+	//type            
+	public static final String MSG_TYPE_PRIVATE_CHAT ="100";//私聊消息
+	public static final String MSG_TYPE_GROUP_CHAT ="200";//群聊消息
+	public static final String MSG_TYPE_NONE="300";//暂无
+	public static final String MSG_TYPE_ADD_MERBER="400";//群成员增加
+	public static final String MSG_TYPE_DEL_MERBER="410";// 群成员减少
+	public static final String MSG_TYPE_ADD_FRIEND="500";//收到好友请求
+	public static final String MSG_TYPE_RECEIVABLES="600";//二维码收款
+	public static final String MSG_TYPE_TRANSFER="700";//收到转账
+	public static final String MSG_TYPE_START="800";//软件开始启动
+	public static final String MSG_TYPE_START_END="900";//新的账号登录完成
+	public static final String MSG_TYPE_OUT ="910";//账号下线
+	
+	
+	public static final String MSG_SUBTYPE_SYS="10000";//系统消息
+	public static final String MSG_SUBTYPE_TEXT="1";//文本消息
+	public static final String MSG_SUBTYPE_IMG="3";//图片消息
+	public static final String MSG_SUBTYPE_VOICE="34";//语音消息
+	public static final String MSG_SUBTYPE_LINK="49";//链接消息
+
+	
+}
