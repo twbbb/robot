a2cc08e4637164d54e106b8fb73b87911313fa6b
Administrator <Administrator@windows10.microdone.cn>
2020-02-03 01:13:15 +0800

    tijiao

--- robot-server/src/main/java/com/twb/robot/server/msgsendhandler/imp/LovelyCatGroupInviteMsgHandler.java
+++ robot-server/src/main/java/com/twb/robot/server/msgsendhandler/imp/LovelyCatGroupInviteMsgHandler.java
@@ -1,11 +1,16 @@
 package com.twb.robot.server.msgsendhandler.imp;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.twb.robot.bean.SendHandlerContext;
+import com.twb.robot.common.config.RobotCommonConstants;
 import com.twb.robot.common.config.RobotSendConstants;
+import com.twb.robot.common.entity.MessageSend;
+import com.twb.robot.common.utils.StringConvertUtils;
 import com.twb.robot.config.LovelyCatConstants;
+import com.twb.robot.server.RobotServerManager;
 import com.twb.robot.server.msgsendhandler.BaseLovelyCatMsgSendHandler;
 import com.twb.robot.server.msgsendhandler.IMessageSendHandler;
 
@@ -20,11 +25,36 @@ public class LovelyCatGroupInviteMsgHandler extends BaseLovelyCatMsgSendHandler{
 
 	@Override
 	public void handlerMyMessageSend(SendHandlerContext sendHandlerContext) {
+		String groupId = sendHandlerContext.getMessageSend().getToGroupId();
+		String  robotId= sendHandlerContext.getMessageSend().getLocalRobotId();
+		MessageSend messageSend = new MessageSend();
+		messageSend.setMsgType(RobotSendConstants.MSG_TYPE_GROUPMEMBERLIST);
+		messageSend.setToGroupId(groupId);
+		messageSend.setLocalRobotId(robotId);
+		
+		RobotServerManager.getRobotServer().handlerSendMsg(messageSend);
+		String state = messageSend.getSendState();
+		String msg = messageSend.getSendStateMsg();//the content is null
+		if(!RobotCommonConstants.MESSAGE_SEND_SUC_STATE.equals(state)){
+			return;
+		}
+		String friendId = sendHandlerContext.getMessageSend().getToUserId();
+		JSONArray ja= (JSONArray) JSON.parse(msg);
+		for(int i=0;i<ja.size();i++){
+			Map map = (Map) ja.get(i);
+			String wxId = StringConvertUtils.toString(map.get("wxid"));
+			if(friendId.equals(wxId)){
+				return;
+			}
+		}
+		
 		Map map = sendHandlerContext.getSendParam();
+		
+		
 		map.put(LovelyCatConstants.MSG_SEND_TYPE, "311");
-		map.put(LovelyCatConstants.MSG_SEND_ROBOT_WXID, sendHandlerContext.getMessageSend().getLocalRobotId());
-		map.put(LovelyCatConstants.MSG_SEND_GROUP_WXID, sendHandlerContext.getMessageSend().getToGroupId()); 
-		map.put(LovelyCatConstants.MSG_SEND_FRIEND_WXID, sendHandlerContext.getMessageSend().getToUserId()); 
+		map.put(LovelyCatConstants.MSG_SEND_ROBOT_WXID, robotId);
+		map.put(LovelyCatConstants.MSG_SEND_GROUP_WXID, groupId); 
+		map.put(LovelyCatConstants.MSG_SEND_FRIEND_WXID, friendId); 
 		
 		
 		
